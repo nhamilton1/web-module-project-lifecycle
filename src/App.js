5282792e101ca087761d75bc5e1d9a6fcf91de75
nhamilton1 <nickh3592@gmail.com>
2021-08-31 15:05:49 -0400

    structed form out, and made handleChange

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    gitUserData: [],
+    username: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      ...this.state,
+      username: e.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Git User Search</h1>
+        <form>
+          <input value={this.state.username} onChange={this.handleChange}/>
+          <button>Search Git User</button>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default App;
